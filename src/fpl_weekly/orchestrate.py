dfbfcb71b53367e0231c648e77da246e351986f5
sarthakshreya <sarthak.29sep@gmail.com>
2025-10-15 21:34:23 +0100

    Added a check in orchestrate.py for remote jobs versus local jobs

--- src/fpl_weekly/orchestrate.py
+++ src/fpl_weekly/orchestrate.py
@@ -11,12 +11,16 @@ print(f"Executing from: {cwd.resolve()}")
 
 data_dir = None
 ts = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H%M")
+is_remote_job = True
 
 try:
-    data_dir = cwd.parent.parent.parent.parent
+    if is_remote_job = True:
+        path = "dbfs:/mnt/mydata"
+    else:
+        data_dir = cwd.parent.parent.parent.parent
     data_dir = data_dir.joinpath(f"fpl_dump_{ts}").joinpath("raw")
 except:
     data_dir = ensure_dir(Path(data_dir) if data_dir else Path(f"./fpl_dump_{ts}/raw"))
 
 output_dir = ext.main(data_dir)
-tr.main(output_dir, True)
+tr.main(output_dir, is_remote_job)
