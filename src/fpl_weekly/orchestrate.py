14d7408e6213b6ac197ec09819be5b0077cc3481
sarthakshreya <sarthak.29sep@gmail.com>
2025-10-15 20:43:37 +0100

    Adding orchestration script

---
+++ src/fpl_weekly/orchestrate.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from datetime import datetime, timezone
+
+import extract_raw_data as ext
+import transform_raw_data as tr
+from util import *
+
+cwd = Path.cwd()
+
+print(f"Executing from: {cwd.resolve()}")
+
+data_dir = None
+ts = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H%M")
+
+try:
+    data_dir = cwd.parent.parent.parent.parent
+    data_dir = data_dir.joinpath(f"fpl_dump_{ts}").joinpath("raw")
+except:
+    data_dir = ensure_dir(Path(data_dir) if data_dir else Path(f"./fpl_dump_{ts}/raw"))
+
+output_dir = ext.main(data_dir)
+tr.main(output_dir, True)
